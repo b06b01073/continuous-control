348d9fae9443bd43a4cb1ce3a08d8bfc0d29f109
b06b01073 <b06b01073@ntu.edu.tw>
2023-04-25 20:37:27 +0800

    lunarlander solved

--- ddpg.py
+++ ddpg.py
@@ -1,44 +1,51 @@
 import torch.nn as nn
 import torch
 import torch.optim as optim
+import numpy as np
+
 
 from random_process import OrnsteinUhlenbeckProcess
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'training on {device}')
 
 class Agent:
-    def __init__(self, obs_dim, action_dim, gamma=0.99, tau=0.01):
+    def __init__(self, obs_dim, action_dim, action_low, action_high, gamma=0.99, tau=0.01):
         self.obs_dim = obs_dim
         self.action_dim = action_dim
         self.gamma = gamma
         self.tau = tau
+        self.action_low = action_low
+        self.action_high = action_high
 
-        print(f'training on {device}')
 
-        self.actor = Actor(input_dim=self.obs_dim, output_dim=self.action_dim).to(device)
+        self.actor = Actor(input_dim=self.obs_dim, output_dim=self.action_dim, scale=np.max(action_high)).to(device)
         self.critic = Critic(input_dim=self.obs_dim + self.action_dim, output_dim=1).to(device)
 
-        self.target_actor = Actor(input_dim=self.obs_dim, output_dim=self.action_dim).to(device)
+        self.target_actor = Actor(input_dim=self.obs_dim, output_dim=self.action_dim, scale=np.max(action_high)).to(device)
         self.target_critic = Critic(input_dim=self.obs_dim + self.action_dim, output_dim=1).to(device)
 
-        self.target_actor.load_state_dict(self.actor.state_dict())
-        self.target_critic.load_state_dict(self.critic.state_dict())
+        for target_param, param in zip(self.target_actor.parameters(), self.actor.parameters()):
+            target_param.data.copy_(param.data)
+
+        for target_param, param in zip(self.target_critic.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param.data)
 
         self.ou_process = OrnsteinUhlenbeckProcess(size=self.action_dim)
 
         self.mse = nn.MSELoss()
 
-        self.critic_optim = optim.Adam(params=self.critic.parameters(), lr=1e-3)
+        self.critic_optim = optim.Adam(params=self.critic.parameters(), lr=1e-2, weight_decay=1e-2)
         self.actor_optim = optim.Adam(params=self.actor.parameters(), lr=1e-3)
 
 
     def step(self, obs):
         with torch.no_grad():
             obs = torch.from_numpy(obs).to(torch.float32).to(device)
-            action = self.actor(obs).to('cpu').numpy()
-
+            action = self.actor(obs).cpu().detach().numpy()
             noise = self.ou_process.sample()
-            return action + noise
+
+            return np.clip(action, a_max=self.action_high, a_min=self.action_low)
         
     def learn(self, experiences):
         if experiences is None:
@@ -48,19 +55,18 @@ class Agent:
         obs, actions, rewards, next_obs, terminated = obs.to(device), actions.to(device), rewards.to(device), next_obs.to(device), terminated.to(device)
         
 
-        self.update_critic(obs, actions, rewards, next_obs, terminated)
         self.update_actor(obs)
-
-        self.soft_update()
-
+        self.update_critic(obs, actions, rewards, next_obs, terminated)
 
     def update_critic(self, obs, actions, rewards, next_obs, terminated):
-        y = rewards + (1 - terminated) * self.gamma * self.target_critic(next_obs, self.target_actor(next_obs).detach()).squeeze().detach()
+        y = rewards + (1 - terminated) * self.gamma * self.target_critic(next_obs, self.target_actor(next_obs)).squeeze().detach()
         q = self.critic(obs, actions).squeeze()
 
         self.critic_optim.zero_grad()
         loss = self.mse(y, q)
         loss.backward()
+
+        nn.utils.clip_grad_norm_(self.critic.parameters(), 1)
         self.critic_optim.step()
 
 
@@ -70,6 +76,7 @@ class Agent:
         
         self.actor_optim.zero_grad()
         grad.backward()
+        nn.utils.clip_grad_norm_(self.actor.parameters(), 1)
         self.actor_optim.step()
 
         
@@ -81,14 +88,16 @@ class Agent:
             target_param.data.copy_(self.tau * param.data + (1.0 - self.tau) * target_param.data)
 
 class Critic(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dim=256):
+    def __init__(self, input_dim, output_dim, hidden_dim=128):
         super().__init__()
+        self.hidden_dim0 = 400
+        self.hidden_dim1 = 200
         self.net = nn.Sequential(
-            nn.Linear(in_features=input_dim, out_features=hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=hidden_dim, out_features=output_dim),
+            nn.Linear(in_features=input_dim, out_features=self.hidden_dim0),
+            nn.ReLU(),
+            nn.Linear(in_features=self.hidden_dim0, out_features=self.hidden_dim1),
+            nn.ReLU(),
+            nn.Linear(in_features=self.hidden_dim1, out_features=output_dim),
         )
 
     def forward(self, obs, action):
@@ -96,15 +105,19 @@ class Critic(nn.Module):
         return self.net(input)
     
 class Actor(nn.Module):
-    def __init__(self, input_dim, output_dim=1, hidden_dim=256):
+    def __init__(self, input_dim, output_dim=1, hidden_dim=128, scale=1):
         super().__init__()
+        self.scale = scale
+        self.hidden_dim0 = 400
+        self.hidden_dim1 = 200
         self.net = nn.Sequential(
-            nn.Linear(in_features=input_dim, out_features=hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=hidden_dim, out_features=hidden_dim),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=hidden_dim, out_features=output_dim),
+            nn.Linear(in_features=input_dim, out_features=self.hidden_dim0),
+            nn.ReLU(),
+            nn.Linear(in_features=self.hidden_dim0, out_features=self.hidden_dim1),
+            nn.ReLU(),
+            nn.Linear(in_features=self.hidden_dim1, out_features=output_dim),
         )
 
     def forward(self, obs):
-        return self.net(obs)
+        obs = self.scale * nn.Tanh()(self.net(obs))
+        return obs
--- train.py
+++ train.py
@@ -1,16 +1,19 @@
 import gym
-import numpy as np
 from argparse import ArgumentParser
-import torch
+from torch.utils.tensorboard import SummaryWriter
 
 from ddpg import Agent
 import replay_buffer
 
+
+writer = SummaryWriter()
+
 def train(args):
-    env = gym.make('Pendulum-v1')
+    env = gym.make('LunarLanderContinuous-v2', continuous=True, render_mode='human')
     action_dim, obs_dim = env.action_space.shape[0], env.observation_space.shape[0]
-    agent = Agent(obs_dim=obs_dim, action_dim=action_dim)
-    buffer = replay_buffer.ReplayBuffer(capacity=10000)
+
+    agent = Agent(obs_dim=obs_dim, action_dim=action_dim, action_low=env.action_space.low, action_high=env.action_space.high)
+    buffer = replay_buffer.ReplayBuffer(capacity=20000)
 
 
     for i in range(args.epoch):
@@ -20,22 +23,24 @@ def train(args):
             action = agent.step(obs)
             next_obs, reward, terminated, _ = env.step(action)
 
+
             buffer.append([obs, action, reward, next_obs, terminated])
 
             obs = next_obs
             agent.learn(buffer.sample(args.batch_size))
+            agent.soft_update()
 
             total_reward += reward
 
             if terminated:
-                env.close()
                 break
-        print(f'epoch {i}: {total_reward}')
+        writer.add_scalar('Total Reward', total_reward, i)
+        print(f'episode {i}: {total_reward}')
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--batch_size', '-b', type=int, default=64)
-    parser.add_argument('--epoch', '-e', type=int, default=1000)
+    parser.add_argument('--batch_size', '-b', type=int, default=256)
+    parser.add_argument('--epoch', '-e', type=int, default=200)
 
     args = parser.parse_args()
 
